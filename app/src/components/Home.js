4ff0bf53b656f13af7ccf08081d14ee6b2f48599
abena07 <62352130+abena07@users.noreply.github.com>
2021-06-09 18:50:12 +0000

    wip

--- app/src/components/Home.js
+++ app/src/components/Home.js
@@ -1,15 +1,35 @@
-import React from 'react'
-import img from "../image/ug.jpg"
+import React,{useState} from 'react'
+// import img from "../image/ug.jpg"
+import ReactMapGL from 'react-map-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+
 
 
 function Home() {
-    return (
-        <div>
-           
+const [viewport, setViewport] = useState
+({
+        width: window.innerWidth,
+        height: window.innerHeight,
+        latitude: 37.7577,
+        longitude: -122.4376,
+        zoom: 8
+              
+    });
     
-           <img src ={img}  className ="center" alt ="ug" width="500"></img>
+    
+       return (
+        <div>
+            
+           
+            <ReactMapGL
+             mapboxApiAccesToken ={"pk.eyJ1IjoibGVzcm9uaWUiLCJhIjoiY2twcHQxanczMHFteTJwbW5mNTJrNGx4dSJ9.PasoedOsW4f0elCegkS_8w"}
+      {...viewport}
+      onViewportChange={nextViewport => setViewport(nextViewport)}
+    />
+          
         </div>
     )
 }
+
 
 export default Home
