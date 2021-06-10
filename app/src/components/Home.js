9c7c670c68231e1a55e099aabd5b10ca0563f42d
abena07 <62352130+abena07@users.noreply.github.com>
2021-06-10 11:04:54 +0000

    wip

--- app/src/components/Home.js
+++ app/src/components/Home.js
@@ -1,35 +1,42 @@
-import React,{useState} from 'react'
-// import img from "../image/ug.jpg"
-import ReactMapGL from 'react-map-gl';
+import React from 'react';
+import '../App.css';
+import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
+import MapboxDirections from '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions'
+import '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions.css'
 
 
+mapboxgl.accessToken = "pk.eyJ1IjoibGVzcm9uaWUiLCJhIjoiY2twcHQxanczMHFteTJwbW5mNTJrNGx4dSJ9.PasoedOsW4f0elCegkS_8w"
 
-function Home() {
-const [viewport, setViewport] = useState
-({
-        width: window.innerWidth,
-        height: window.innerHeight,
-        latitude: 37.7577,
-        longitude: -122.4376,
-        zoom: 8
-              
-    });
-    
-    
-       return (
-        <div>
-            
-           
-            <ReactMapGL
-             mapboxApiAccesToken ={"pk.eyJ1IjoibGVzcm9uaWUiLCJhIjoiY2twcHQxanczMHFteTJwbW5mNTJrNGx4dSJ9.PasoedOsW4f0elCegkS_8w"}
-      {...viewport}
-      onViewportChange={nextViewport => setViewport(nextViewport)}
-    />
-          
-        </div>
-    )
+class Home extends React.Component {
+    componentDidMount() {
+  
+        const map = new mapboxgl.Map({
+          container: this.mapWrapper,
+          style: 'mapbox://styles/mapbox/streets-v10',
+          center: [-0.196224, 5.650562],
+          zoom: 12
+        });
+
+        const directions = new MapboxDirections({
+            accessToken: mapboxgl.accessToken,
+            unit: 'metric',
+            profile: 'mapbox/driving'
+        })
+
+        map.addControl(directions, 'top-left');
+    }
+    render() {
+      return (
+        <>
+        <div 
+            ref={el => (this.mapWrapper = el)} 
+            className="mapWrapper" 
+        />
+        </>
+      );
+    }
 }
 
 
-export default Home
+export default Home;
--- app/src/index.js
+++ app/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import 'mapbox-gl/dist/mapbox-gl.css';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
